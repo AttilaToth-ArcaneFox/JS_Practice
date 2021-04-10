1ec7aa13facf97c20de617b62d0660bb660432d8
Attila <atti.94.tt@gmail.com>
2021-04-10 18:14:47 +0200

    Adedd prime function

--- HtmlJs/function.js
+++ HtmlJs/function.js
@@ -30,12 +30,39 @@ var side2 = prompt('Add meg a téglalap masik oldalát');
 document.write('A téglalap területe: ',areaOfARectangle(side1,side2), ' A Kerülete pedig: ',ThePerimeterOfTheRectangle(side1,side2));
 */
 
+/*
 function areaOfARectangleExtended(a,b){
     var a = prompt('First side');
     var b = prompt('Second side');
     var k = a * b;
     alert ('area Of A Rectangle: ' + k);
 }
+*/
+
+// little bit more function
+/*
+prime function
+function prime(){
+    var prim = prompt('Adj meg egy számot');
+    if (isFinite(prim) && Math.floor(prim) == prim){
+        if(eldont(prim)){
+            alert('A szam prim!');
+        }else{
+            alert(' A szam nem prim!');
+        }
+    }else{
+        alert('Csak egész számot adhatsz meg!');
+    }
+ }   
+
+function eldont(prim){
+    var i = 2;
+    while(i<= Math.floor(Math.sqrt(prim)) && prim % i != 0){
+        ++i;       
+    }
+    return i > Math.floor(Math.sqrt(prim));
+}
+*/
 
 
-
+//https://www.youtube.com/watch?v=-eaB3rRNlLc&list=PLuYHIg_0xQJsiwA4jrXTWaIdqMGJQ4sAU&index=16
