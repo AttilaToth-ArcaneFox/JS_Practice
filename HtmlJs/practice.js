8b36a8634bf7b7bc675fa1dc475db9ad9f6d920b
Attila <atti.94.tt@gmail.com>
2021-04-12 14:03:07 +0200

    Added wondow function

---
+++ HtmlJs/practice.js
@@ -0,0 +1,20 @@
+// window object
+
+/*
+var window = {
+    log: function(msg){
+        //log msg
+    }
+};
+*/
+
+// globális objektum pl window
+//window.console.log('Szia');
+
+//change the color
+//document.getElementById('Hello').style.backgroundColor = "#FF0000";
+
+document.all[6].onclick = function(){
+    document.getElementById('Hello').style.backgroundColor = "#FF0000";
+    console.log('ok');
+}
