42057ab04b1f52ca66509dfb503735a6c025a67d
Attila <atti.94.tt@gmail.com>
2021-04-13 13:08:08 +0200

    boxes

--- JSNew/script.js
+++ JSNew/script.js
@@ -2,13 +2,76 @@
 1. doboz:
 Kattintásra adjunk hozzá egy "blur" nevű class attribútumot, majd újabb kattintásra vegyük
 le róla azt.
+
+    // - element JS reprezentáció : document.getElementById('element-one')
+    // - esemény kiválasztása : onclick 
+    // - funcionalitás leirása : function () {...}
+
 */
+// 1. doboz
+
+// document.getElementById('element-one') kiválassszuk azt amin dolgozni szeretnénk,
+// jelen esetben ez az 1. kocka 
+// classList.add('saját class') hozzáadjuk azt amit akarunk csinálni onclick eseményre. 
+// jelen esetben a saját class = blur a cssben
+
+//belső állapot: state 
+
+var isBluerd = false;
+
+// történi egy esemény pl gombnyomás elindit egy eseményt 
+document.getElementById('element-one').onclick = function(){ 
+
+    // state change történik
+    isBluerd = !isBluerd;   
+
+    //renderelés 
+    if (isBluerd){
+
+    document.getElementById('element-one').classList.add('blur');
+
+    }else{
+
+        document.getElementById('element-one').classList.remove('blur');
+    }
+}
+
 
 /*
 2. doboz:
 Ha az egérrel fölé megyünk változzon meg a háttérszíne pirosra, ha levesszük róla az egeret
 változzon vissza az eredeti színére.
 */
+// 2
+
+// state
+var isMouseOverTheElement = false;
+
+
+// action
+document.getElementById('element-two').onmouseover = function(){     
+    //state change
+    isMouseOverTheElement = true;
+    renderSecondBox();
+                
+}
+
+// action 
+document.getElementById('element-two').onmouseout = function(){
+    //state change
+    isMouseOverTheElement = false;
+    renderSecondBox();
+
+}
+function renderSecondBox(){
+    if (isMouseOverTheElement){
+                document.getElementById('element-two').style.backgroundColor = 'red';
+    }else{
+        document.getElementById('element-two').style.backgroundColor = '';
+    }
+}
+
+//https://www.youtube.com/watch?v=CE7xq9tmC98&list=PL6VA7Z5zO0dq-6NWrHzIy7dqLfeZhhhOz&index=15
 
 /*
 3. doboz:
